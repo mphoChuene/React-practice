4e80338acebd64816ad8c1b5573ec15fa7c537d5
mphoChuene <mphochuene42@gmail.com>
2023-08-05 14:29:15 +0200

    usestate hook practice complete

--- src/components/usestatehook/State.jsx
+++ src/components/usestatehook/State.jsx
@@ -2,20 +2,24 @@ import React,{useState} from "react";
 
 const State = () => {
 
-  const [text,setText]= useState('')
+  const [color,setColor]=useState('black')
+  const [visible,setVisible]= useState(true)
 
-  // const [task,setTask] = useState('')
-const handleText =(event)=>{
-    setText(event.target.value)
-}
- 
+  const handleChange =()=>{
+    setColor(color === 'black' ? 'red' : 'black')
+  }
+
+  const visibility =()=>{
+    setVisible(!visible)
+  }
 
   return (
     <div>
-      <h2>usestate hook:</h2>
-      <input type="text" placeholder="some text..." onChange={handleText} />
-
-      {text}
+      {/* change the color of the text */}
+      {visible &&<h2 style={{color:color}}>hooks practice:</h2>}
+      <button onClick={handleChange}>change</button>
+      <button onClick={visibility}>hide/show</button>
+ 
      
     </div>
   );
