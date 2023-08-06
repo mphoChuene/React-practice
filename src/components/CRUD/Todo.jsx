0c1f0d9eca5d587903cac6b8b9a90eb7ebe3c36e
mphoChuene <mphochuene42@gmail.com>
2023-08-06 17:52:31 +0200

    CRUD delete function

--- src/components/CRUD/Todo.jsx
+++ src/components/CRUD/Todo.jsx
@@ -1,30 +1,43 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 const Todo = () => {
-    const [todoList,setTodoList]=useState([])
-    const [task,setTask]=useState('')
+  const [todoList, setTodoList] = useState([]);
+  const [task, setTask] = useState("");
 
-    const handleInput=(event)=>{
-        setTask(event.target.value)
-    }
-
-    const addTask =()=>{
-        setTodoList([...todoList,task])
-    }
-
+  const handleInput = (event) => {
+    setTask(event.target.value);
+  };
+  const addTask = () => {
+    setTodoList([...todoList, task]);
+  };
+  const deleteTask=(taskName)=>{
+        const newTodoList = todoList.filter((task)=>{
+            if (taskName===task){
+                return false
+            } else {
+                return true
+            }
+        })
+        setTodoList(newTodoList)
+  }
   return (
     <div>
-        <div className="inputs">
-            <input type="text" onChange={handleInput} />
-            <button onClick={addTask}>add</button>
-        </div>
-        <div className="task">
-            {todoList.map((task)=>{
-                return <h4>{task}</h4>
-            })}
-        </div>
+      <div className="inputs">
+        <input type="text" onChange={handleInput} />
+        <button onClick={addTask}>add</button>
+      </div>
+      <div className="display">
+        {todoList.map((task) => {
+          return (
+            <div className="container">
+              <h4>{task}</h4>
+              <button onClick={()=>deleteTask(task)}>delete</button>
+            </div>
+          );
+        })}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Todo
+export default Todo;
